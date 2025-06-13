8bdbbd7ec3a38c0cafe90d9957fd4e01b2425bb2
Yugendran Sunder <yugendran.sunder@gmail.com>
2025-06-13 14:38:50 +0530

    WebAPI Updated folder name

--- Day-30 13-06-2025/MYAPP/src/app/login/login.ts
+++ Day-30 13-06-2025/MYAPP/src/app/login/login.ts
@@ -10,11 +10,11 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './login.css'
 })
 export class Login {
-user:UserLoginModel = new UserLoginModel();
-constructor(private userService:UserService){
+  user:UserLoginModel = new UserLoginModel();
+  constructor(private userService:UserService){
 
-}
-handleLogin(){
-  this.userService.validateUserLogin(this.user);
-}
+  }
+  handleLogin(){
+    this.userService.validateUserLogin(this.user);
+  }
 }
